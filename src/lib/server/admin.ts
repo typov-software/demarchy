352f5ce03fdd9d5972813b9c38ed606348035e9b
David Glivar <davidglivar@gmail.com>
2023-11-17 09:09:15 -0800

    double quoting key

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -8,7 +8,7 @@ try {
 		credential: pkg.credential.cert({
 			projectId: FB_PROJECT_ID,
 			clientEmail: FB_CLIENT_EMAIL,
-			privateKey: FB_PRIVATE_KEY
+			privateKey: `"${FB_PRIVATE_KEY}"`
 		})
 	});
 } catch (err: unknown) {
