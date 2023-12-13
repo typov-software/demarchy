d8df9d387e513643aa2f0f7609d8b1f7f4054539
David Glivar <davidglivar@gmail.com>
2023-12-12 17:29:05 -0800

    tests: fix playwright test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,54 +5,56 @@
 <div class="hero min-h-screen bg-base-200">
   <div class="hero-content text-center">
     <div class="max-w-md">
-      <svg width="100%" viewBox="0 0 606 115" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M237 82V34H249V22H273V34H285V82H273V58H249V82H237ZM249 46H273V34.48H249V46Z"
-          class="fill-primary"
-        />
-        <path
-          d="M309 82V22H345V34H357V46H345V70H357V82H345V70H333V58H321V82H309ZM321 46H344.52V34H321V46Z"
-          class="fill-primary"
-        />
-        <path
-          d="M393 34V22H417V34H393ZM393 82V70H381V34H393V70H417V82H393ZM417 70V58H429V70H417Z"
-          class="fill-primary"
-        />
-        <path d="M453 82V22H465V46H489V22H501V82H489V58H465V82H453Z" class="fill-primary" />
-        <path
-          d="M549 82V46H537V34H525V22H537V34H549V46H561V82H549ZM573 34V22H585V34H573ZM561 46V34H573V46H561Z"
-          class="fill-primary"
-        />
-        <path
-          d="M237 94V46L249 46V34H273V46L285 46V94H273V70H249V94H237ZM249 58H273V46.48H249V58Z"
-          class="fill-secondary"
-        />
-        <path
-          d="M309 94V34H345V46H357V58H345V82H357V94H345V82H333V70H321V94H309ZM321 58L344.52 58V46H321V58Z"
-          class="fill-secondary"
-        />
-        <path
-          d="M393 46V34H417V46H393ZM393 94V82H381V46H393V82H417V94H393ZM417 82V70H429V82H417Z"
-          class="fill-secondary"
-        />
-        <path d="M453 94V34H465V58H489V34H501V94H489V70H465V94H453Z" class="fill-secondary" />
-        <path
-          d="M549 94V58H537V46L525 46V34H537V46H549V58H561V94H549ZM573 46V34H585V46L573 46ZM561 58V46H573V58H561Z"
-          class="fill-secondary"
-        />
-        <path d="M21 82V22H57V34H69V70H57V82H21ZM33 70H56.52V34H33V70Z" class="fill-secondary" />
-        <path d="M93 82V22H129V34H105V46H129V58H105V70H129V82H93Z" class="fill-secondary" />
-        <path
-          d="M153 82V22H165V34H177V46H189V58H177V46H165V82H153ZM201 82V46H189V34H201V22H213V82H201Z"
-          class="fill-secondary"
-        />
-        <path d="M21 94V34H57V46H69V82H57V94H21ZM33 82H56.52V46H33V82Z" class="fill-primary" />
-        <path d="M93 94V34H129V46H105V58H129V70H105V82H129V94H93Z" class="fill-primary" />
-        <path
-          d="M153 94V34H165V46H177V58H189V70H177V58L165 58V94H153ZM201 94V58L189 58V46H201V34H213V94H201Z"
-          class="fill-primary"
-        />
-      </svg>
+      <h1 title="Demarchy">
+        <svg width="100%" viewBox="0 0 606 115" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M237 82V34H249V22H273V34H285V82H273V58H249V82H237ZM249 46H273V34.48H249V46Z"
+            class="fill-primary"
+          />
+          <path
+            d="M309 82V22H345V34H357V46H345V70H357V82H345V70H333V58H321V82H309ZM321 46H344.52V34H321V46Z"
+            class="fill-primary"
+          />
+          <path
+            d="M393 34V22H417V34H393ZM393 82V70H381V34H393V70H417V82H393ZM417 70V58H429V70H417Z"
+            class="fill-primary"
+          />
+          <path d="M453 82V22H465V46H489V22H501V82H489V58H465V82H453Z" class="fill-primary" />
+          <path
+            d="M549 82V46H537V34H525V22H537V34H549V46H561V82H549ZM573 34V22H585V34H573ZM561 46V34H573V46H561Z"
+            class="fill-primary"
+          />
+          <path
+            d="M237 94V46L249 46V34H273V46L285 46V94H273V70H249V94H237ZM249 58H273V46.48H249V58Z"
+            class="fill-secondary"
+          />
+          <path
+            d="M309 94V34H345V46H357V58H345V82H357V94H345V82H333V70H321V94H309ZM321 58L344.52 58V46H321V58Z"
+            class="fill-secondary"
+          />
+          <path
+            d="M393 46V34H417V46H393ZM393 94V82H381V46H393V82H417V94H393ZM417 82V70H429V82H417Z"
+            class="fill-secondary"
+          />
+          <path d="M453 94V34H465V58H489V34H501V94H489V70H465V94H453Z" class="fill-secondary" />
+          <path
+            d="M549 94V58H537V46L525 46V34H537V46H549V58H561V94H549ZM573 46V34H585V46L573 46ZM561 58V46H573V58H561Z"
+            class="fill-secondary"
+          />
+          <path d="M21 82V22H57V34H69V70H57V82H21ZM33 70H56.52V34H33V70Z" class="fill-secondary" />
+          <path d="M93 82V22H129V34H105V46H129V58H105V70H129V82H93Z" class="fill-secondary" />
+          <path
+            d="M153 82V22H165V34H177V46H189V58H177V46H165V82H153ZM201 82V46H189V34H201V22H213V82H201Z"
+            class="fill-secondary"
+          />
+          <path d="M21 94V34H57V46H69V82H57V94H21ZM33 82H56.52V46H33V82Z" class="fill-primary" />
+          <path d="M93 94V34H129V46H105V58H129V70H105V82H129V94H93Z" class="fill-primary" />
+          <path
+            d="M153 94V34H165V46H177V58H189V70H177V58L165 58V94H153ZM201 94V58L189 58V46H201V34H213V94H201Z"
+            class="fill-primary"
+          />
+        </svg>
+      </h1>
       <p class="py-6">A democratic document editor for organizations and communities.</p>
       <div class="flex flex-row gap-4 justify-center">
         <a href="/join" class="btn btn-primary">Join now</a>
