26847b841616102a96c33b4741c3eecd8bf5211f
David Glivar <davidglivar@gmail.com>
2023-12-13 13:30:55 -0800

    refactor: add handle functions to string.ts
    Moved handle validity checks to testable function.

--- src/lib/utils/string.test.ts
+++ src/lib/utils/string.test.ts
@@ -1,5 +1,5 @@
 import { expect, describe, test } from 'vitest';
-import { checkValidSlug } from './string';
+import { checkValidHandle, checkValidSlug } from './string';
 
 describe('string.ts', () => {
   test('checkValidSlug() returns true for a valid slug', () => {
@@ -16,5 +16,22 @@ describe('string.ts', () => {
     // 32 character limit on slug length
     expect(checkValidSlug('012345678901234567890123456789012')).toBe(false);
     expect(checkValidSlug('01')).toBe(false);
+  });
+
+  test('checkValidHandle() returns true for a valid user handle', () => {
+    expect(checkValidHandle('typovdev')).toBe(true);
+    expect(checkValidHandle('typov_dev')).toBe(true);
+    expect(checkValidHandle('typov_dev_000')).toBe(true);
+  });
+
+  test('checkValidHandle() returns false for an invalid user handle', () => {
+    expect(checkValidHandle('Typov_Dev')).toBe(false);
+    expect(checkValidHandle('typov-dev')).toBe(false);
+    expect(checkValidHandle('typov__dev')).toBe(false);
+    expect(checkValidHandle('typov_dev_')).toBe(false);
+    expect(checkValidHandle('typov_dev-')).toBe(false);
+    // 32 character limit on slug length
+    expect(checkValidHandle('012345678901234567890123456789012')).toBe(false);
+    expect(checkValidHandle('01')).toBe(false);
   });
 });
