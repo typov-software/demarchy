b4aa0f8269102f5087d487c7cef2011e8f745a95
David Glivar <davidglivar@gmail.com>
2023-12-19 13:24:44 -0800

    feat: add comments interface
    Add info to comments interface

--- src/lib/models/comments.ts
+++ src/lib/models/comments.ts
@@ -1,14 +1,19 @@
 import type { WithId } from './utils';
 
-export type CommentContext = 'proposal' | 'poll';
+export type CommentContext = 'feedback' | 'proposal' | 'poll';
 
+// https://fireship.io/courses/firestore-data-modeling/models-tree/
 export interface CommentProps {
   context: CommentContext;
+  /**
+   * This should be the ID of the contextual document linked to this comment. In the case of
+   * a context without a document, like "feedback", use the comment id here.
+   */
   context_id: string;
   parent: string | null;
   depth: number;
+  user_id: string | null; // allow anonymous comments
+  created_at: Date;
 }
 
 export type Comment = CommentProps & WithId;
-
-// https://fireship.io/courses/firestore-data-modeling/models-tree/
