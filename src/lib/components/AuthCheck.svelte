c1d58f439c3d55465404b6b7c39b0b5e4f3ebd0b
David Glivar <davidglivar@gmail.com>
2023-12-24 21:41:42 -0800

    update authcheck component styles

--- src/lib/components/AuthCheck.svelte
+++ src/lib/components/AuthCheck.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
   import { user } from '$lib/firebase';
+  import BasicSection from './BasicSection.svelte';
 </script>
 
 {#if $user}
   <slot />
 {:else}
-  <p class="text-error">
-    You must be signed in to view this page.
-    <a class="btn btn-primary" href="/login">Sign in</a>
-  </p>
+  <BasicSection otherClass="min-h-screen h-full hero justify-center">
+    <div class="hero-content bg-base-200 rounded-xl p-8">
+      <p class="text-error flex flex-col items-center gap-4">
+        You must be signed in to view this page.
+        <a class="btn btn-primary" href="/login">Sign in</a>
+      </p>
+    </div>
+  </BasicSection>
 {/if}
