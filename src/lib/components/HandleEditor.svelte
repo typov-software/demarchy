74edb4b5f2ccadad9ee494ff1e2ad851c0ce9357
David Glivar <davidglivar@gmail.com>
2024-02-06 17:10:00 -0800

    better handle editor messaging

--- src/lib/components/HandleEditor.svelte
+++ src/lib/components/HandleEditor.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import { db } from '$lib/firebase';
   import { HANDLES } from '$lib/models/firestore';
-  import { checkValidHandle } from '$lib/utils/string';
+  import { checkValidHandle, emptyString } from '$lib/utils/string';
   import { doc, getDoc } from 'firebase/firestore';
+  import ProfileLink from './ProfileLink.svelte';
+  import { working } from '$lib/stores/working';
 
+  export let showLoading = true;
   export let currentHandle: string;
   let handle = currentHandle;
   let loading = false;
@@ -15,6 +18,8 @@
   $: isTouched = handle.length > 0;
   $: isTaken = isValid && !isAvailable && !loading;
   $: isChanged = currentHandle !== handle;
+  $: disabled =
+    $working.length > 0 || !isAvailable || loading || !isValid || !isTouched || !isChanged;
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async function checkHandleAvailability(e: any) {
@@ -23,6 +28,11 @@
     clearTimeout(debounceTimer);
     loading = true;
     debounceTimer = setTimeout(async () => {
+      if (emptyString(handle)) {
+        isAvailable = false;
+        loading = false;
+        return;
+      }
       const ref = doc(db, HANDLES, handle.toLowerCase());
       const exists = await getDoc(ref).then((doc) => doc.exists());
       isAvailable = !exists;
@@ -33,47 +43,59 @@
 
 <div class="flex flex-col gap-4">
   <div class="flex items-end gap-4 w-full">
-    <div class="flex flex-col flex-1 items-start justify-end">
-      <label for="handle">Handle</label>
-      <input
-        type="text"
-        id="handle"
-        name="handle"
-        value={handle}
-        autocomplete="off"
-        class="input input-bordered w-full"
-        on:input={checkHandleAvailability}
-        class:input-error={!isValid && isTouched}
-        class:input-warning={isTaken && isChanged}
-        class:input-success={isAvailable && isValid && !loading && isTouched && isChanged}
-      />
+    <div class="flex flex-col flex-1 items-start justify-end gap-2">
+      <label for="handle" class="text-sm text-neutral">Handle</label>
+      <div class="w-full relative">
+        <span
+          class="absolute flex justify-center items-center w-8 h-full pointer-events-none text-neutral"
+          >@</span
+        >
+        <input
+          type="text"
+          id="handle"
+          name="handle"
+          value={handle}
+          autocomplete="off"
+          class="input input-bordered w-full pl-7"
+          on:input={checkHandleAvailability}
+          class:input-error={!isValid && isTouched}
+          class:input-warning={isTaken && isChanged}
+          class:input-success={isAvailable && isValid && !loading && isTouched && isChanged}
+        />
+      </div>
     </div>
-    <button
-      type="submit"
-      class="btn btn-primary"
-      disabled={!isAvailable || loading || !isValid || !isTouched || !isChanged}
-    >
+    <button type="submit" class="btn btn-primary" {disabled}>
+      {#if showLoading && $working.length}
+        <span class="loading loading-sm" />
+      {/if}
       Save
     </button>
   </div>
-  <div class="w-full text-left">
+  <div class="w-full text-left pl-3">
     {#if loading}
-      <p class="text-secondary text-sm">Checking availability of @{handle}...</p>
+      <p class="text-sm">
+        Checking availability of <span class="text-neutral">@{handle}</span>...
+      </p>
     {/if}
 
     {#if !isValid && isTouched}
-      <p class="text-error text-sm">must be 3-32 characters long, alphanumeric only</p>
+      <p class="text-error text-sm">must be 3-32 characters long, alphanumeric only.</p>
     {/if}
 
     {#if isValid && !isAvailable && !loading && isChanged}
       <p class="text-warning text-sm">
-        @{handle} is not available
+        @{handle} is not available.
       </p>
     {/if}
 
     {#if !loading && !isChanged}
-      <p class="text-accent text-sm">
-        @{handle} is your handle, rejoice!
+      <p class="text-sm">
+        <ProfileLink {handle} />
+        is your handle.
+      </p>
+    {:else if !disabled}
+      <p class="text-sm">
+        <span class="text-success">@{handle}</span> is available, press Save.
       </p>
     {/if}
   </div>
