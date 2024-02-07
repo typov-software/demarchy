14e34d8b121900bfd5b9b13e75c0d28d2bc591a8
David Glivar <davidglivar@gmail.com>
2024-02-06 17:11:17 -0800

    improve join layouts and messaging

--- src/routes/join/profile/+page.svelte
+++ src/routes/join/profile/+page.svelte
@@ -9,42 +9,62 @@
 
   export let data: PageData;
 
+  $: updatingName = false;
   $: profileName = data.profile?.name ?? '';
   $: name = profileName ?? '';
-  $: disabled = !data.profile || (!emptyString(profileName) && profileName === name);
+  $: disabled =
+    updatingName || !data.profile || (!emptyString(profileName) && profileName === name);
   $: user_id = $user?.uid;
 
   async function confirmName() {
-    await setDoc(doc(db, 'profiles', user_id!), { name }, { merge: true });
-    await invalidateAll();
+    if (updatingName) return;
+    updatingName = true;
+    try {
+      await setDoc(doc(db, 'profiles', user_id!), { name }, { merge: true });
+      await invalidateAll();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      updatingName = false;
+    }
   }
 </script>
 
 <AuthCheck>
-  <form class="w-full flex flex-row items-center" on:submit|preventDefault={confirmName}>
-    <div class="flex flex-1 w-full">
+  <p class="w-full max-w-sm text-left self-start pb-4">
+    Choose how others will see
+    <span class="demarchy-faded-walk">you</span>
+  </p>
+  <form class="w-full flex flex-row items-end" on:submit|preventDefault={confirmName}>
+    <div class="flex flex-1 flex-col items-start w-full gap-2">
+      <label for="name" class="text-sm text-neutral">Name</label>
       <input
         type="text"
         id="name"
         autocomplete="name"
         placeholder="Name"
-        class="input w-full"
+        class="input input-bordered w-full"
         bind:value={name}
       />
     </div>
-    <button {disabled} type="submit" class="btn btn-success ml-5">Confirm</button>
+    <button {disabled} type="submit" class="btn btn-primary ml-5">
+      {#if updatingName}
+        <span class="loading loading-sm" />
+      {/if}
+      Save
+    </button>
   </form>
 
-  <div class="max-w-screen-md w-full">
-    <div class="form-control w-full max-w-xs my-4 mx-auto text-center">
-      <img
-        src={data.profile?.photo_url ?? '/user.png'}
-        alt="photo_url"
-        class="mx-auto w-full mb-4"
-      />
+  <div class="w-full">
+    <div class="form-control w-full my-4 mx-auto text-center gap-4">
       {#if user_id}
         <ProfilePhotoEditor profileId={user_id} />
       {/if}
+      <img
+        src={data.profile?.photo_url ?? '/user.png'}
+        alt="photo_url"
+        class="mx-auto w-full max-w-xs mb-4 avatar rounded-full"
+      />
     </div>
   </div>
 
