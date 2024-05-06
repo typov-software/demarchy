3218eaf67963927659247cfa8d7ad2a214989b50
David Glivar <davidglivar@gmail.com>
2024-05-05 20:32:15 -0700

    fix: use bun test instead of vite

--- packages/community/vite.config.ts
+++ packages/community/vite.config.ts
@@ -1,9 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}'],
+  server: {
+    port: 3000,
   },
+  plugins: [sveltekit()],
 });
